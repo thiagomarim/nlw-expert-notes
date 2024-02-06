4a67db5f266d13bbffac6d2d92f0ae66080af520
Thiago <thiago.marim2005@gmail.com>
2024-02-06 09:51:58 -0300

    Adicionado o modal da nota

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
+const note = {
+  date: new Date(),
+  content: "Hello Word",
+};
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
@@ -19,7 +24,7 @@ export function App() {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
       </div>
     </div>
   );
