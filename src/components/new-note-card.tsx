8c61041ed069a4ad407da2ff3bd618325ba9ce54
Thiago <thiago.marim2005@gmail.com>
2024-02-07 21:22:57 -0300

    Adicionado a criação de nota

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,7 +3,11 @@ import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from "sonner";
 
-export function NewNoteCard() {
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void;
+}
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true);
   const [content, setContent] = useState("");
 
@@ -21,6 +25,11 @@ export function NewNoteCard() {
 
   function handleSaveNote(event: FormEvent) {
     event.preventDefault();
+
+    onNoteCreated(content);
+
+    setContent("");
+    setShouldShowOnBoarding(true);
 
     toast.success("Nota criada com sucesso");
   }
@@ -67,6 +76,7 @@ export function NewNoteCard() {
                   autoFocus
                   className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
                   onChange={handleContentChanged}
+                  value={content}
                 />
               )}
             </div>
